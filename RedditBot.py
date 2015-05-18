ece32e6af80233183020bbbda1fae95a30960484
CoreyHickson <corey.r.hickson@gmail.com>
2015-05-17 21:18:46 -0400

    Working bot which responds to 'hello'

--- RedditBot.py
+++ RedditBot.py
@@ -1,11 +1,12 @@
 # based off of Reddit user /u/Scotty_the_Hamster and praw documentation
 import praw
+import time
 
-
-r = praw.Reddit(user_agent = "Reactions by Corey, /u/Poruku.")
+r = praw.Reddit(user_agent = "Reactions gifs by /u/Poruku.")
+print "[!] Logging in..."
 r.login()
 
-words_to_match = ["!dora"]
+words_to_match = ["hello"]
 cache = []
 
 def run_bot():
@@ -15,6 +16,11 @@ def run_bot():
 	for comment in comments:
 		comment_text = comment.body.lower()
 		is_match = any(string in comment_text for string in words_to_match)
-		if is_match:
+		if comment.id not in cache and is_match:
 			comment.reply("[Hola!](http://www.reactiongifs.com/r/dora.gif)")
 			cache.append(comment.id)
+			print "Match found: %s" % comment.body
+
+while True:
+	run_bot()
+	time.sleep(10)
